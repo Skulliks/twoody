4c5ad8615c44b8b2df9c5d3927f327a20ce34eff
Shevtsov Denis <ShevtsovD1@s2.rpn.ch>
2025-06-06 16:43:48 +0200

    Litle part of script for hiding/unhiding the sidebar

--- scripts/main_script.js
+++ scripts/main_script.js
@@ -8,3 +8,19 @@ function showNotification(message) {
         setTimeout(() => notification.style.visibility = 'hidden', 500);
     }, 3000);
 }
+
+isOpen = true;
+
+const sidebar = document.getElementById('sidebar')
+const content_sidebar = document.getElementById('content_sidebar')
+const openSidebar = document.getElementById('openSidebar')
+
+if(isOpen == false){
+    openSidebar.style.marginLeft = '12.5%';
+    sidebar.style.width = '12%';
+    content_sidebar.style.visibility = 'visible';
+}
+
+function openSidebar(){
+    isOpen = false;
+}
